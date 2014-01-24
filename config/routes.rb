431529cd098e5c0d77ec6c36746b2e4761255840
Evan Humphreys <humphreyse1@nku.edu>
2014-01-24 18:02:42 +0000

    5.8, although not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 NkuRails::Application.routes.draw do
   resources :posts
   
-  root to: "welcome#index"
-  #get "welcome/index"
+  root to: "posts#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
