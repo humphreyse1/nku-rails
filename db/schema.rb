e79133869f33dfc27420a690e9053e10aef6a1bf
Evan Humphreys <humphreyse1@nku.edu>
2014-02-07 17:16:42 +0000

    Run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124174031) do
+ActiveRecord::Schema.define(version: 20140207170832) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -26,4 +26,5 @@ ActiveRecord::Schema.define(version: 20140124174031) do
     t.string "email"
     t.string "image_url"
   end
+
 end
