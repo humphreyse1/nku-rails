3cee355f517a656c56d0f9453458fd449dd6410f
Evan Humphreys <humphreyse1@nku.edu>
2014-02-08 03:04:08 +0000

    Move getImage back to students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,6 +20,19 @@ class StudentsController < ApplicationController
     redirect_to students_path, notice: "Successfully updated #{@student.name}"
   end
   
+  #get gravatar image
+  def getImage
+    if @student.image==nil
+      @student.image= "http://www.gravatar.com/avatar"
+      if @student.email != nil
+        require 'digest/md5'
+        hash= Digest::MD5.hexdigest(@student.email).downcase
+        @student.image= "http://www.gravatar.com/avatar/#{hash}"
+      end
+    end
+  end
+  helper_method :getImage
+  
   def index
     @students= Student.all
   end
--- app/helpers/student_helper.rb
+++ app/helpers/student_helper.rb
@@ -1,13 +1,3 @@
 module StudentHelper
-  #get gravatar image
-  def getImage
-    if @student.image==nil
-      @student.image= "http://www.gravatar.com/avatar"
-      if @student.email != nil
-        require 'digest/md5'
-        hash= Digest::MD5.hexdigest(@student.email).downcase
-        @student.image= "http://www.gravatar.com/avatar/#{hash}"
-      end
-    end
-  end
+  
 end
