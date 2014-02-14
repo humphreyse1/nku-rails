27092c04f02af8d07989b835bf1a1cabc95c6cac
Evan Humphreys <humphreyse1@nku.edu>
2014-02-14 16:53:11 +0000

    Comment line causing error

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,12 +22,12 @@ class StudentsController < ApplicationController
   
   #get gravatar image
   def getImage
-    if @student.image==nil
-      @student.image= "http://www.gravatar.com/avatar"
+    if @student.image_url==nil
+      @student.image_url= "http://www.gravatar.com/avatar"
       if @student.email != nil
         require 'digest/md5'
         hash= Digest::MD5.hexdigest(@student.email).downcase
-        @student.image= "http://www.gravatar.com/avatar/#{hash}"
+        @student.image_url= "http://www.gravatar.com/avatar/#{hash}"
       end
     end
   end
