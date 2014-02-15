8f1fa94ef55c6e21c8930349c20eb28844d877dc
Evan Humphreys <humphreyse1@nku.edu>
2014-02-15 00:45:47 +0000

    Update students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,28 +2,26 @@ class StudentsController < ApplicationController
   def new
     @student= Student.new
   end
-  
+
   def create
     @student= Student.create!(student_params)
-    session[:id]= @student.id
-    flash[:notice]= "You have successfully created #{@student.name}."
-    redirect_to students_path, notice: "Hi, #{@student.name}."
+    session[:student_id]= @student.id
+    redirect_to students_path, notice: "Hi, #{@student.name}!"
   end
-  
+
   def edit
     @student= current_student
   end
-  
+
   def update
-    @student= Student.find(params[:id])
-    @student.update_attributes!(student_params)
-    redirect_to students_path, notice: "Successfully updated #{@student.name}"
+    current_student.update_attributes!(student_params)
+    redirect_to students_path, notice: "Successfully updated your profile."
   end
-  
+
   def index
     @students= Student.all
   end
-  
+
   private
   def student_params
     params.require(:student).permit!
