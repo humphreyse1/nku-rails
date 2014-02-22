4c75e658e3637a49d74b524f4d0460cab6ea705a
Evan Humphreys <humphreyse1@nku.edu>
2014-02-22 01:24:41 +0000

    Add sessions_controller.rb

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def create
+    student= Student.find_by(email: session_params[:email])
+    if student && student.authenticate(session_params[:password])
+      session[:student_id]= student.id
+      redirect_to students_path, notice: "Signed in!"
+    else
+      flash[:error]= "Invalid e-mail and/or password."
+      render :new
+    end
+  end
+  
+  def destroy
+    session[:student_id]= nil
+    redirect_to root_path, notice: "Successfully signed out."
+  end
+  
+  private
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
