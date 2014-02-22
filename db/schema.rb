854e7858847b9c72d571efc1aab27f4dbe7da87d
Evan Humphreys <humphreyse1@nku.edu>
2014-02-22 02:29:32 +0000

    Add simple lines

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140207223120) do
+ActiveRecord::Schema.define(version: 20140222022656) do
 
   create_table "attendances", force: true do |t|
     t.string "attendance"
